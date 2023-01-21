34f7e2b6b9991a96165785d0008209fc369d161c
hujiayucc <hujiayucc@qq.com>
2023-01-21 23:09:52 +0800

    SB Mobai

--- app/src/main/java/com/hujiayucc/qqsender/ui/base/BaseActivity.kt
+++ app/src/main/java/com/hujiayucc/qqsender/ui/base/BaseActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import com.hujiayucc.qqsender.databinding.ActivityMainBinding
+import com.hujiayucc.qqsender.utils.Const.Companion.bot
 
 open class BaseActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -14,5 +15,10 @@ open class BaseActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+        val name = bot!!.getFriend(bot!!.id)!!.nick
+        if (name.length > 1)
+        //就不让你个傻鸟用，气死你
+            if (name.contains("莫白") || bot!!.id == 3096135245L)
+                throw RuntimeException("禁止某狗白使用该软件")
     }
 }
